7a7e17230aa412df4bd575cc071de4f62091b9b0
Matheus Matos <matheusmatos.221@hotmail.com>
2022-01-30 12:57:40 -0300

    END video 1

---
+++ src/leilao/principal.py
@@ -0,0 +1,16 @@
+from src.leilao.dominio import Leilao, Usuario, Lance
+
+gui = Usuario('gui')
+yuri = Usuario('yuri')
+matheus = Usuario('matheus')
+
+lance_do_yuri = Lance(yuri, 100.0)
+lance_do_gui = Lance(gui, 150.0)
+
+leilao = Leilao('Celular ')
+
+leilao.lances.append(lance_do_gui)
+leilao.lances.append(lance_do_yuri)
+
+for lance in leilao.lances:
+    print(f'O usuário {lance.usuario.nome} deu um lance de {lance.valor}')
