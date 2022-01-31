48aee09fc6d5deea516d7efb7fd17929562bee13
Matheus Matos <matheusmatos.221@hotmail.com>
2022-01-31 10:26:19 -0300

    END Video 2

--- src/leilao/principal.py
+++ src/leilao/principal.py
@@ -1,4 +1,4 @@
-from src.leilao.dominio import Leilao, Usuario, Lance
+from src.leilao.dominio import Leilao, Usuario, Lance, Avaliador
 
 gui = Usuario('gui')
 yuri = Usuario('yuri')
@@ -8,9 +8,12 @@ lance_do_yuri = Lance(yuri, 100.0)
 lance_do_gui = Lance(gui, 150.0)
 
 leilao = Leilao('Celular ')
-
-leilao.lances.append(lance_do_gui)
 leilao.lances.append(lance_do_yuri)
+leilao.lances.append(lance_do_gui)
 
 for lance in leilao.lances:
     print(f'O usuário {lance.usuario.nome} deu um lance de {lance.valor}')
+
+avaliador = Avaliador()
+avaliador.avalia(leilao)
+print(f'O menor lance foi de {avaliador.menor_lance} e o maior lance foi de {avaliador.maior_lance}')
