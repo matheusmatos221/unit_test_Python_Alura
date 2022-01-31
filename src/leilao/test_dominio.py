167666da682586f5d4b50c42ed3838bd6a6186c0
Matheus Matos <matheusmatos.221@hotmail.com>
2022-01-31 15:17:47 -0300

    Mais testes e classes de equivalência

--- src/leilao/test_dominio.py
+++ src/leilao/test_dominio.py
@@ -7,7 +7,6 @@ class TestAvaliador(TestCase):
     def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionamos_em_ordem_crescente(self):
         gui = Usuario('gui')
         yuri = Usuario('yuri')
-        matheus = Usuario('matheus')
 
         lance_do_yuri = Lance(yuri, 100.0)
         lance_do_gui = Lance(gui, 150.0)
@@ -28,7 +27,6 @@ class TestAvaliador(TestCase):
     def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionamos_em_ordem_decrescente(self):
         gui = Usuario('gui')
         yuri = Usuario('yuri')
-        matheus = Usuario('matheus')
 
         lance_do_yuri = Lance(yuri, 100.0)
         lance_do_gui = Lance(gui, 150.0)
@@ -45,3 +43,41 @@ class TestAvaliador(TestCase):
 
         self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
         self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+
+    def test_deve_retornar_o_mesmo_valor_para_o_maior_e_menor_lance_quando_leilao_tiver_apenas_um_lance(self):
+        gui = Usuario('gui')
+
+        lance = Lance(gui, 100.0)
+
+        leilao = Leilao('teste')
+        leilao.lances.append(lance)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        self.assertEqual(100.0, avaliador.menor_lance)
+        self.assertEqual(100.0, avaliador.maior_lance)
+
+    def test_deve_retornar_o_maior_e_o_menor_valor_quando_o_leilao_tiver_tres_lances(self):
+        gui = Usuario('gui')
+        yuri = Usuario('yuri')
+        vini = Usuario('vini')
+
+        lance_do_yuri = Lance(yuri, 100.0)
+        lance_do_gui = Lance(gui, 150.0)
+        lance_do_vini = Lance(vini, 200.0)
+
+        leilao = Leilao('Celular ')
+        leilao.lances.append(lance_do_yuri)
+        leilao.lances.append(lance_do_gui)
+        leilao.lances.append(lance_do_vini)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 200.0
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+
